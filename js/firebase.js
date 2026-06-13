8f457f3ed24be21b0fddb289bee494f298e5e26a
Aman288228 <sagarkushwaha61@gmail.com>
2026-06-13 17:53:09 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, increment, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyD0apa8kCYrCn7C2XvJQ4VlY4kTNQKqnlM",
+  authDomain: "sagarsocial-93251.firebaseapp.com",
+  projectId: "sagarsocial-93251",
+  storageBucket: "sagarsocial-93251.firebasestorage.app",
+  messagingSenderId: "967483981455",
+  appId: "1:967483981455:web:6d3fcca8556b5f22247afa"
+};
 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
