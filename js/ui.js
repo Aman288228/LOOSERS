148672bbdceb1ca8979743b2393ddcf43f23242d
Aman288228 <sagarkushwaha61@gmail.com>
2026-06-13 19:18:28 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,6 @@
+const writeBlogBtn = document.getElementById("writeBlogBtn");
+const authModal = document.getElementById("authModal");
 
+writeBlogBtn.onclick = () => {
+  authModal.style.display = "flex";
+};
