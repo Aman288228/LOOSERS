29fe5155877c6b0211dc018ec3c92fe7fefc1a5b
Aman288228 <sagarkushwaha61@gmail.com>
2026-06-13 18:34:14 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,42 @@
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "./firebase.js";
 
+let currentUser = null;
+
+document.getElementById("signupBtn").onclick = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    document.getElementById("msg").innerText = "Signup Successful!";
+  } catch (e) {
+    document.getElementById("msg").innerText = e.message;
+  }
+};
+
+document.getElementById("loginBtn").onclick = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    document.getElementById("msg").innerText = "Login Successful!";
+  } catch (e) {
+    document.getElementById("msg").innerText = e.message;
+  }
+};
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = user;
+    console.log("Logged In:", user.email);
+  } else {
+    currentUser = null;
+    console.log("No User Logged In");
+  }
+});
