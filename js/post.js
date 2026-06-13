7b16e93e82acc7b0b236b485f19daa36606b3ea3
Aman288228 <sagarkushwaha61@gmail.com>
2026-06-13 18:55:35 +0530

    Update post.js

--- js/post.js
+++ js/post.js
@@ -7,3 +7,27 @@ import {
   updateDoc,
   increment
 } from "./firebase.js";
+async function loadPosts() {
+  const postsDiv = document.getElementById("posts");
+  postsDiv.innerHTML = "";
+
+  const querySnapshot = await getDocs(collection(db, "posts"));
+
+for (const doc of querySnapshot.docs) {
+  const postId = doc.id;
+  const post = doc.data();
+
+  postsDiv.innerHTML += `
+    <div style="border:1px solid #ccc;padding:10px;margin:10px;">
+      <h3>${post.title}</h3>
+      <p>${post.content}</p>
+
+      <button onclick="likePost('${postId}')">❤️ ${post.likes || 0}</button>
+      <input type="text" id="comment-${postId}" placeholder="Write a comment">
+      <button onclick="addComment('${postId}')">💬 Comment</button>
+
+      <div></div>
+    </div>
+  `;
+ }
+}
