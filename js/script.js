305ccc0329f10331b38d9af525637e0ca4ecf40b
Aman288228 <sagarkushwaha61@gmail.com>
2026-06-13 19:13:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,59 +37,11 @@ let currentUser = null;
     document.getElementById("postMsg").innerText = e.message;
   }
 }; */
-async function loadPosts() {
-  const postsDiv = document.getElementById("posts");
-  postsDiv.innerHTML = "";
 
-  const querySnapshot = await getDocs(collection(db, "posts"));
-
-for (const doc of querySnapshot.docs) {
-  const postId = doc.id;
-  const post = doc.data();
-
-  postsDiv.innerHTML += `
-    <div style="border:1px solid #ccc;padding:10px;margin:10px;">
-      <h3>${post.title}</h3>
-      <p>${post.content}</p>
-
-      <button onclick="likePost('${postId}')">❤️ ${post.likes || 0}</button>
-      <input type="text" id="comment-${postId}" placeholder="Write a comment">
-      <button onclick="addComment('${postId}')">💬 Comment</button>
-
-      <div></div>
-    </div>
-  `;
- }
-}
 
 loadPosts();
-window.likePost = async function(postId) {
-  const postRef = doc(db, "posts", postId);
 
-  await updateDoc(postRef, {
-    likes: increment(1)
-  });
 
-  loadPosts();
-}
-
-window.addComment = async function(postId) {
-
-  const text =
-    document.getElementById(`comment-${postId}`).value;
-
-  if (!text) {
-    alert("Comment likho");
-    return;
-  };
-
-  await addDoc(collection(db, "comments"), {
-    postId: postId,
-    text: text
-  });
-
-  alert("Comment Added!");
-}
 const writeBlogBtn = document.getElementById("writeBlogBtn");
 const authModal = document.getElementById("authModal");
 
@@ -97,3 +49,4 @@ writeBlogBtn.onclick = () => {
   authModal.style.display = "flex";
 };
 import "./auth.js";
+import "./posts.js";
