0a5996c416d7ca6392ce3fa0626a3ea680c8201c
Aman288228 <sagarkushwaha61@gmail.com>
2026-07-02 14:36:20 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,22 @@
+import { auth } from "./firebase.js";
+
 console.log("UI Loaded");
+
 const writeBlogBtn = document.getElementById("writeBlogBtn");
 const authModal = document.getElementById("authModal");
 
 writeBlogBtn.onclick = () => {
-  authModal.style.display = "flex";
+
+  if (auth.currentUser) {
+
+    // User Login Hai
+    alert("Blog Editor Open Hoga (Next Step)");
+
+  } else {
+
+    // User Login Nahi Hai
+    authModal.style.display = "flex";
+
+  }
+
 };
